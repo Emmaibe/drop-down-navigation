a857df0b015c309fdc019a5391e1bc2a7a23ef3c
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-05-05 23:53:46 +0100

    side-nav update

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const navDropDown = document.querySelector(".mobile-features");
 const navDropDownC = document.querySelector(".mobile-company");
 const rt1 = document.querySelector(".r1");
 const rt2 = document.querySelector(".r2");
+const navToggle = document.querySelectorAll(".mobile-nav-toggle");
+const sideNav = document.querySelector(".side-nav");
 
 featureListener.addEventListener("click", () => {
   navDropDown.toggleAttribute("visible");
@@ -13,4 +15,10 @@ featureListener.addEventListener("click", () => {
 companyListener.addEventListener("click", () => {
   navDropDownC.toggleAttribute("visible");
   rt2.toggleAttribute("rotate");
+});
+
+navToggle.forEach((navListen) => {
+  navListen.addEventListener("click", () => {
+    sideNav.toggleAttribute("visible");
+  });
 });
