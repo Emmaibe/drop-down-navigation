c8b660c98de7ebd6aabdeb31aed6beadfae9b140
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-05-05 22:27:25 +0100

    js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+const listener = document.querySelector(".mobile-drop-down");
+const navDropDown = document.querySelector(".mobile-features");
+const rotateImg = document.querySelector(".rt-img");
 
+listener.addEventListener("click", () => {
+  navDropDown.toggleAttribute("visible");
+  rotateImg.toggleAttribute("rotate");
+});
